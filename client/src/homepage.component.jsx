bc5881bbbd572c0f7c3f2b83f1b18fd22dee6e37
Rhett Harrison <reharri7@asu.edu>
2021-07-12 19:06:40 -0700

    add the homepage

---
+++ client/src/homepage.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+const HomePage = () => (
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>IPAS</div>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>SOURS</div>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>STOUTS</div>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>SELTZERS</div>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>MILKSHAKES</div>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+);
+
+export default HomePage;
